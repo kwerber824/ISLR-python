16dd5ddfd7be128b114095e6430913ccaa5ffbed
Kurt <werber.kr@gmail.com>
2023-01-08 12:04:31 -0500

    stats models

--- src/chapter3lab.py
+++ src/chapter3lab.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as smf
 
 # 3.6 Lab: Linear Regression
 
@@ -14,5 +16,9 @@ print(X[0:10])
 singleRegression = LinearRegression().fit(X, y)
 print(singleRegression.coef_)
 print(singleRegression.intercept_)
+statmodel = smf.ols('medv ~ lstat', boston).fit()
+print(statmodel.summary())
+prediction = singleRegression.predict(np.array([5, 10, 15]).reshape(-1, 1))
+print(prediction)
 
 X = boston.drop('medv', axis=1)
